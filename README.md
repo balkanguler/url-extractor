b0036590a24ec2fbe2249aa4dfd072471036ad96
balkan <balkanguler@gmail.com>
2021-01-02 15:35:53 +0300

    fixed redirection bug for urls start with www.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # url-extractor
 Extracts URL from image and redirects to this URL
+
+
+Uses these environment variables to call AWS services.
+
+AWS_ACCESS_KEY_ID
+AWS_SECRET_ACCESS_KEY
+AWS_REGION
+
+in your local aws_config.json file can be used. Its structure:
+{
+    "accessKeyId": "...",
+    "secretAccessKey":  "...",
+    "region": "us-east-1"
+}
--- app.js
+++ app.js
@@ -7,8 +7,9 @@ var logger = require('morgan');
 var AWS = require("aws-sdk");
 
 var app = express();
-
+//AWS.config.loadFromPath("./aws_config.json");
 const rekognition = new AWS.Rekognition();
+
 
 const storage = multer.memoryStorage();
 const urlRegexExp = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s][^)]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s][^)]{2,}|www\.[a-zA-Z0-9]+\.[^\s][^)]{2,})/gi;
@@ -32,7 +33,10 @@ app.post('/', (req, res) => {
                 for (var i = 0; i < data.TextDetections.length; i++) {
                     var matches = data.TextDetections[i].DetectedText.match(regex)
                     if (matches) {
-                        return res.status(301).redirect(matches[0]);
+                        var url = matches[0];
+                        if (!url.startsWith("https") && !url.startsWith("http"))
+                            url = "http://" + url;
+                        return res.status(301).redirect(url);
                     }
                 }
                 res.redirect("/?message=No result found! Please try again.");
